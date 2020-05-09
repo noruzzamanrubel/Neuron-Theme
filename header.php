eefcfb980d2511344641f173e6760c354755a154
noruzzaman rubel <noruzzamanrubel@gmail.com>
2020-05-09 22:03:40 +0600

    Update

--- header.php
+++ header.php
@@ -51,25 +51,9 @@ wp_head();
 
 	        <!-- Start Navigation -->
 	        <nav class="navbar navbar-default navbar-sticky bootsnav">
-	            <!-- Start Top Search -->
-	            <div class="top-search">
-	                <div class="container">
-	                    <div class="input-group">
-						<?php get_search_form( ); ?>
-	                    </div>
-	                </div>
-				</div>
-	            <!-- End Top Search -->
+
 
 	            <div class="container">
-	                <!-- Start Atribute Navigation -->
-	                <div class="attr-nav">
-	                    <ul>
-	                        <li class="search"><a href="#"><i class="fa fa-search"></i></a></li>
-	                    </ul>
-	                </div>
-	                <!-- End Atribute Navigation -->
-
 	                <!-- Start Header Navigation -->
 	                <div class="navbar-header">
 	                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
